9493f743f900a80912a8d8e962e5c06133af6313
Jared Anderson <jared.anderson@tangocard.com>
2023-12-15 14:21:26 -0700

    Finished day 8 with more recursion

--- src/advent_of_code/day8/day8.py
+++ src/advent_of_code/day8/day8.py
@@ -1,11 +1,10 @@
-from operator import countOf
-from os.path import dirname
+import math
 import re
-import sys
+from functools import reduce
+from os.path import dirname
 
 from advent_of_code.utils import helpers
-from advent_of_code.utils.DictList import Dictlist
-sys.setrecursionlimit(100000)
+
 
 def parse_map(map: list[str]) -> dict:
     parsed_map: dict = {}
@@ -23,6 +22,7 @@ def parse_map(map: list[str]) -> dict:
 def traverse_map_as_camel(desert_map: dict, node: str = 'AAA', traversals: int = 0, iteration: int = 1) -> int:
     for direction in desert_map['directions']:
         node_lookup = desert_map[node]
+
         if direction == 'L':
             node = node_lookup[0]
         elif direction == 'R':
@@ -36,29 +36,32 @@ def traverse_map_as_camel(desert_map: dict, node: str = 'AAA', traversals: int =
 
     return traversals
 
-# def traverse_map_as_ghost(desert_map: dict, nodes: list[str] = [], traversals: int = 0, iteration: int = 1) -> int:
-#     # print(iteration)
-#     if nodes == []:
-#         nodes = list(filter(lambda node: node.endswith('A'), list(desert_map.keys())))
+def traverse_map_as_ghost(desert_map: dict, initial_nodes: list[str] = []) -> int:
+    end_traversals = []
+    initial_nodes = list(filter(lambda node: node.endswith('A'), list(desert_map.keys())))
 
-#     for node in nodes:
-#         for direction in desert_map['directions']:
-#             temp_nodes = []
-#             if direction == 'L':
-#                 for node in nodes:
-#                     temp_nodes.append(desert_map[node][0])
-#             elif direction == 'R':
-#                 for node in nodes:
-#                     temp_nodes.append(desert_map[node][1])
-#             nodes = temp_nodes
-#             traversals += 1
+    for i in range(len(initial_nodes)):
+        traversal = traverse_node_as_ghost(desert_map, initial_nodes[i])
+        end_traversals.append(traversal)
 
-#         if all(node.endswith('Z') for node in nodes):
-#             return traversals
-#         if traversals == len(desert_map['directions']) * iteration:
-#             return traverse_map_as_ghost(desert_map, nodes, traversals, iteration + 1)
+    return reduce(lambda x, y: math.lcm(x, y), end_traversals)
 
-#     return traversals
+def traverse_node_as_ghost(desert_map: dict, node: str = 'AAA', traversals: int = 0, iteration: int = 1) -> tuple[str, int]:
+    for direction in desert_map['directions']:
+        node_lookup = desert_map[node]
+        if direction == 'L':
+            node = node_lookup[0]
+        elif direction == 'R':
+            node = node_lookup[1]
+        traversals += 1
+
+        if node.endswith('Z'):
+            return traversals
+        if traversals == len(desert_map['directions']) * iteration:
+            return traverse_node_as_ghost(desert_map, node, traversals, iteration + 1)
+
+    return traversals
+
 
 if __name__ == '__main__':
     input_file = f'{dirname(__file__)}/desert_map.txt'
@@ -66,5 +69,5 @@ if __name__ == '__main__':
 
     desert_map = parse_map(initial_input)
     camel_traversals = traverse_map_as_camel(desert_map)
-    # ghost_traversals = traverse_map_as_ghost(desert_map)
-    print(camel_traversals)
+    ghost_traversals = traverse_map_as_ghost(desert_map)
+    print(camel_traversals, ghost_traversals)
